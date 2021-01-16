6185fde74c01e4bc19fa4f2d50a3ab480818b912
Arley517693777 <55181721+Arley517693777@users.noreply.github.com>
2021-01-16 09:23:41 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -97,7 +97,7 @@ head_addon = r'''
 $(function() {
    $(".yue figure img").each(function(i) {
       if (!this.parentNode.href) {
-         $(this).wrap("<a href='" + this.src + "' data-fancybox='images' data-caption='figcaption" + this.alt + "'></a>")
+         $(this).wrap("<a href='" + this.src + "' data-fancybox='images' data-caption='" + this.figcaption + "'></a>")
       }
    })
 });
